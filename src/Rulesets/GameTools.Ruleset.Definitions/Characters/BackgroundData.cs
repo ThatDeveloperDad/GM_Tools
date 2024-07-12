b4f38594ba367e3e482121603c8a74b48e237c3f
Rob McCrady <rob.mccrady@gmail.com>
2024-07-12 14:21:30 -0400

    Added Backgrounds as a data source to our RuleSets.  Implemented for 5eSRD with a little help from ChatGPT.  GithubIssue #29

---
+++ src/Rulesets/GameTools.Ruleset.Definitions/Characters/BackgroundData.cs
@@ -0,0 +1,36 @@
+﻿namespace GameTools.Ruleset.Definitions;
+
+public abstract class BackgroundData : IGameOptionSet<BackgroundTemplate>
+{
+
+    protected readonly List<BackgroundTemplate> _backgroundTemplates;
+
+    public BackgroundData()
+    {
+        _backgroundTemplates = new List<BackgroundTemplate>();
+        InitializeTemplates();
+    }
+
+    protected void RegisterBackground(BackgroundTemplate background)
+    {
+        _backgroundTemplates.Add(background);
+    }
+
+    protected abstract void InitializeTemplates();
+
+    public string[] List()
+    {
+        var backgroundList = _backgroundTemplates
+            .Select(b=> b.Name)
+            .ToArray();
+
+        return backgroundList;
+    }
+
+    public BackgroundTemplate? Load(string optionName)
+    {
+        var ret = _backgroundTemplates
+            .FirstOrDefault(x => x.Name == optionName);
+        return ret;
+    }
+}
---
+++ src/Rulesets/GameTools.Ruleset.Definitions/Characters/BackgroundTemplate.cs
@@ -0,0 +1,17 @@
+﻿namespace GameTools.Ruleset.Definitions;
+
+public class BackgroundTemplate
+{
+
+    public BackgroundTemplate()
+    {
+        Name = string.Empty;
+    }
+
+    public BackgroundTemplate(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; init; }
+}
