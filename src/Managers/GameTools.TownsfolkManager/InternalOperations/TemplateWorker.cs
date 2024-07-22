66b99199e1a63a93fe0f8922228c3e2f69b4ecda
Rob McCrady <rob.mccrady@gmail.com>
2024-07-22 11:08:26 -0400

    Adds Gender options (default and customizable per species) and automatic selection when applying the Species Template to the NPC

--- src/Managers/GameTools.TownsfolkManager/InternalOperations/TemplateWorker.cs
+++ src/Managers/GameTools.TownsfolkManager/InternalOperations/TemplateWorker.cs
@@ -1,4 +1,5 @@
-﻿using GameTools.Ruleset.Definitions;
+﻿using GameTools.DiceEngine;
+using GameTools.Ruleset.Definitions;
 using GameTools.Ruleset.Definitions.Characters;
 using GameTools.TownsfolkManager.Contracts;
 using System;
@@ -12,19 +13,34 @@ namespace GameTools.TownsfolkManager.InternalOperations
 {
     internal static class TemplateWorker
     {
-        public static Townsperson ApplySpecies(this Townsperson npc, SpeciesTemplate template)
+        public static Townsperson ApplySpecies(this Townsperson npc, 
+                                                    SpeciesTemplate template,
+                                                    ICardDeck shuffler,
+                                                    IDiceBag dice)
         {
             npc.Species = template.Name;
+
+            // Choose Gender from the options on the Template.
+            string genderOption = shuffler.PickOne(template.GenderOptions);
+            npc.Appearance.Gender = genderOption;
+            npc.Pronouns = template.GetPronouns(genderOption);
+
             return npc;
         }
 
-        public static Townsperson ApplyBackground(this Townsperson npc, BackgroundTemplate template)
+        public static Townsperson ApplyBackground(this Townsperson npc, 
+                                                       BackgroundTemplate template,
+                                                       ICardDeck shuffler,
+                                                       IDiceBag dice)
         {
             npc.Background.Name = template.Name;
             return npc; 
         }
 
-        public static Townsperson ApplyVocation(this Townsperson npc, VocationTemplate template)
+        public static Townsperson ApplyVocation(this Townsperson npc, 
+                                                     VocationTemplate template,
+                                                     ICardDeck shuffler,
+                                                     IDiceBag dice)
         {
             npc.Vocation.Name = template.Name;
             return npc; 
--- src/Rulesets/GameTools.Ruleset.Definitions/Characters/SpeciesTemplate.cs
+++ src/Rulesets/GameTools.Ruleset.Definitions/Characters/SpeciesTemplate.cs
@@ -1,18 +1,74 @@
-﻿namespace GameTools.Ruleset.Definitions.Characters
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameTools.Ruleset.Definitions.Characters
 {
     // Defines the properties that are common to the concept of a TTRPG Species.
     public class SpeciesTemplate
     {
-        public SpeciesTemplate()
-        {
-            Name = string.Empty;
-        }
+        public readonly string[] DefaultGenderChoices = [ "Ambiguous", "Female", "Male" ];
+        public readonly Dictionary<string, string> DefaultPronounMapping;
+        public readonly List<string> _customGenderOptions;
+        public readonly Dictionary<string, string> _customPronounMappings;
+
+        public SpeciesTemplate():this(string.Empty) { }
 
         public SpeciesTemplate(string name)
         {
             Name = name;
+            _customGenderOptions = new List<string>();
+            _customPronounMappings = new Dictionary<string, string>();
+
+            DefaultPronounMapping = new Dictionary<string, string>();
+            DefaultPronounMapping["Ambiguous"] = "They/Them/Theirs";
+            DefaultPronounMapping["Female"] = "She/Her/Hers";
+            DefaultPronounMapping["Male"] = "He/Him/His";
         }
         
         public string Name { get; init; }
+
+        public void AddGenderOptions(Tuple<string, string>[] genderOptions)
+        {
+            var genders = genderOptions.Select(x => x.Item1).ToList();
+
+            _customGenderOptions.AddRange(genders);
+
+            foreach(var option in genderOptions)
+            {
+                _customPronounMappings[option.Item1] = option.Item2;
+            }
+        }
+
+        public void AddGenderOption(string genderOption, string pronouns)
+        {
+            _customGenderOptions.Add(genderOption);
+            _customPronounMappings[genderOption] = pronouns;
+        }
+
+        public string[] GenderOptions
+        {
+            get
+            {
+                var options = _customGenderOptions.Any() == true
+                                ? _customGenderOptions.ToArray()
+                                : DefaultGenderChoices;
+
+                return options;
+            }
+        }
+
+        public string GetPronouns(string gender)
+        {
+            string pronouns = DefaultPronounMapping["Ambiguous"];
+            if (_customPronounMappings.ContainsKey(gender))
+            {
+                pronouns = _customPronounMappings[gender];
+            }
+            else if(DefaultPronounMapping.ContainsKey(gender)) 
+            {
+                pronouns = DefaultPronounMapping[gender];
+            }
+
+            return pronouns;
+        }
     }
 }
