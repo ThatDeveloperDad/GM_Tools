48143ffcacfa454c01d9d5350db854167b549456
Rob McCrady <rob.mccrady@gmail.com>
2024-07-30 18:00:31 -0400

    Adds a "loading" screen while the AI is workin on the character description. #62

---
+++ src/UI/GameTools.BlazorClient/GameTools.BlazorClient/Components/Pages/NpcGeneratorComponent.cs
@@ -0,0 +1,55 @@
+﻿using GameTools.UI.Components.Wrapper;
+using GameTools.BlazorClient.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace GameTools.BlazorClient.Components.Pages
+{
+    public class NpcGeneratorComponent:ComponentBase
+    {
+        [CascadingParameter] 
+        protected WaitWrapper? WaitWrapper { get; set; }
+
+        protected DisplayNPCViewModel NPC = new DisplayNPCViewModel();
+
+        [Inject]
+        protected NpcViewModelManager npcVmMgr { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            GenerateNpc();
+        }
+
+        protected void GenerateNpc()
+        {
+            DisplayNPCViewModel npcViewModel = npcVmMgr.GenerateRandomNPC();
+            NPC = npcViewModel;
+        }
+
+        protected async Task OnAiButtonClick()
+        {
+            try
+            {
+                NPC = await WaitWrapper.AwaitTask(GetAiDescription());
+            }
+            catch (Exception e)
+            {
+                // Don't ever do this.  I'm only try catching to see what's actually happening.
+                throw;
+            }
+        }
+        private async Task<string> FakeTask()
+        {
+            await Task.Delay(3000);
+            return string.Empty;
+        }
+
+        private async Task<DisplayNPCViewModel> GetAiDescription()
+        {
+
+            var newModel = await npcVmMgr.GetAiDescription(NPC);
+            return newModel??NPC;
+
+        }
+    }
+}
